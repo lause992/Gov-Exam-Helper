ffbee4984526adebe574117ad5faa736d1025ef9
Lause <lause@users.noreply.gitee.com>
2026-08-10 20:36:59 +0800

    bisect10

---
+++ tmp/t_i.js
@@ -0,0 +1,13 @@
+﻿) + '</span></div></div>' +
+          '<span class="news-star on" data-act="unsaveNews" data-title="' + esc(item.title) + '" title="取消收藏">★</span>' +
+          '</div>';
+        html += '</div>';
+      });
+    }
+    html += '</div></div>';
+    return html;
+  }
+
+  function getFallbackNews() {
+    var today = new Date();
+    
