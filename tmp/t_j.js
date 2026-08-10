38bc183afd882bf2b5aefd41d4543a78fa60264f
Lause <lause@users.noreply.gitee.com>
2026-08-10 20:44:02 +0800

    清理测试文件

--- tmp/t_j.js
+++
@@ -1,6 +0,0 @@
-﻿var m = today.getMonth() + 1;
-    var d = today.getDate();
-    return [
-      { title: '2026年政府工作报告要点', source: '人民日报', time: m + '月' + d + '日', summary: '报告强调高质量发展，推进科技创新和产业升级。' },
-      { title: '国务院常务会议部署重点工作', source: '新华社', time: m + '月' + d + '日', summary: '会议研究部署进一步扩大内需、促进消费的政策措施。' },
-      { title: '乡村振兴战略深入推进', sou
